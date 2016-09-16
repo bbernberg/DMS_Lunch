8c6d99a9f35d72ea1f89e6c983a4a61ac8a2e961
Brian Bernberg <brian.bernberg@gamechanger.io>
2016-09-15 22:17:53 -0400

    Add Thumbs Down field to restaurant (and migrate database)

--- DMS_Lunch/lunch/models.py
+++ DMS_Lunch/lunch/models.py
@@ -7,15 +7,15 @@ class Restaurant(models.Model):
 	latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 	longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 	visited = models.BooleanField(default=0)
+	thumbs_down_by_users = models.ManyToManyField(User)
 	def __str__(self):
 		return self.name
 
 class Review(models.Model):
 	text = models.TextField()
 	date = models.DateTimeField('date posted')
-	thumbs_down = models.BooleanField(default=0)
 	restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	def __str__(self):
 		return self.text
 
@@ -23,5 +23,6 @@ class ReviewComment(models.Model):
 	text = models.TextField()
 	date = models.DateTimeField('date posted')
 	parent_review = models.ForeignKey(Review, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	def __str__(self):
 		return self.text

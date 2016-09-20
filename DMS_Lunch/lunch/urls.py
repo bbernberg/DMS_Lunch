52dcd1840b7f62bd782c4587dcd22c1d677556e9
Brian Bernberg <brian.bernberg@gamechanger.io>
2016-09-19 22:52:34 -0400

    CSS styling changes

--- DMS_Lunch/lunch/urls.py
+++ DMS_Lunch/lunch/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
 	url(r'^$', views.dashboard, name='dashboard'),
 	url(r'^restaurant/(?P<restaurant_id>[0-9]+)/$', views.restaurant, name='restaurant'),
 	url(r'^(?P<restaurant_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
-	url('^register/', CreateView.as_view(
-            template_name='registration/register.html',
-            form_class=UserCreationForm
-    ), name='register'),
+	url('^register/', CreateView.as_view(template_name='registration/register.html', form_class=UserCreationForm), name='register'),
     url('^do-register/', views.do_register, name='do_register'),   
     url(r'^(?P<review_id>[0-9]+)/add_review_comment/$', views.add_review_comment, name='add_review_comment'),
     url(r'^(?P<restaurant_id>[0-9]+)/update_thumbs_down/$', views.update_thumbs_down, name='update_thumbs_down'),
+    url(r'^(?P<restaurant_id>[0-9]+)/mark_visited/$', views.mark_restaurant_visited, name='mark_restaurant_visited'),
+    url(r'^add-restaurant/', views.add_restaurant, name='add_restaurant'),
+    url(r'^do-add-restaurant/', views.do_add_restaurant, name='do_add_restaurant'),
 ]
--- DMS_Lunch/lunch/views.py
+++ DMS_Lunch/lunch/views.py
@@ -12,15 +12,29 @@ from lunch.models import Restaurant, Review, ReviewComment
 @login_required
 def dashboard(request):
 	restaurants = Restaurant.objects.exclude(thumbs_down_by_users__pk=request.user.pk)
+	visited_restaurants = list()
+	unvisited_restaurants = list()
+	for restaurant in restaurants:
+		if restaurant.visited:
+			visited_restaurants.append(restaurant)
+		else:
+			unvisited_restaurants.append(restaurant)
 	thumbs_down_restaurants = Restaurant.objects.filter(thumbs_down_by_users__pk=request.user.pk)
-	return render(request, 'lunch/dashboard.html', {'user': request.user, 'restaurants': restaurants, 'thumbs_down_restaurants': thumbs_down_restaurants})
+	return render(request, 'lunch/dashboard.html', 
+		{'user': request.user, 
+		'visited_restaurants': visited_restaurants, 
+		'unvisited_restaurants': unvisited_restaurants,
+		'thumbs_down_restaurants': thumbs_down_restaurants})
 
 @login_required
 def restaurant(request, restaurant_id):
 	restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
 	reviews = Review.objects.filter(restaurant__pk=restaurant_id)
 	thumbs_down = restaurant.thumbs_down_by_users.all().filter(pk=request.user.pk).exists()
-	return render(request, 'lunch/restaurant.html', {'restaurant': restaurant, 'reviews': reviews, 'thumbs_down': thumbs_down})
+	return render(request, 'lunch/restaurant.html', 
+		{'restaurant': restaurant, 
+		'reviews': reviews, 
+		'thumbs_down': thumbs_down})
 
 def add_review(request, restaurant_id):
 	restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
@@ -30,7 +44,10 @@ def add_review(request, restaurant_id):
 
 def add_review_comment(request, review_id):
 	review = get_object_or_404(Review, pk=review_id)
-	review_comment = ReviewComment(text=request.POST['new_review_comment'], date=timezone.now(), parent_review=review, author=request.user)
+	review_comment = ReviewComment(text=request.POST['new_review_comment'], 
+		date=timezone.now(), 
+		parent_review=review, 
+		author=request.user)
 	review_comment.save()
 	review.comments.add(review_comment)
 	review.save()
@@ -50,3 +67,19 @@ def update_thumbs_down(request, restaurant_id):
 		restaurant.thumbs_down_by_users.add(request.user)
 	restaurant.save()
 	return HttpResponseRedirect(reverse('restaurant', args=restaurant_id))
+
+def mark_restaurant_visited(request, restaurant_id):
+	restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+	restaurant.visited = True
+	restaurant.save()
+	return HttpResponseRedirect(reverse('restaurant', args=restaurant_id))
+
+@login_required
+def add_restaurant(request):
+	return render(request, 'lunch/add_restaurant.html')	
+
+def do_add_restaurant(request):
+	restaurant = Restaurant(name=request.POST['name'], address=request.POST['address'])
+	restaurant.save()
+	return HttpResponseRedirect(reverse('dashboard'))
+
